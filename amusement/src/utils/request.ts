2970eb197b3dd557917f2ffea9e69ed953fe9650
tomchen <chenzijian@jurassic.com.cn>
2023-10-20 11:27:04 +0800

    fix:精简代码

--- amusement/src/utils/request.ts
+++ amusement/src/utils/request.ts
@@ -1,12 +1,23 @@
 /*
  * @Date: 2023-10-20 10:45:39
  * @LastEditors: 陈子健
- * @LastEditTime: 2023-10-20 11:08:20
+ * @LastEditTime: 2023-10-20 11:25:06
  * @FilePath: /amusement/src/utils/request.ts
  */
 import { Method } from "axios"
 
-import myRequest from './axios'
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: process.env.NODE_ENV === 'development' ? '/api' : '',
+  withCredentials: true, // send cookies when cross-domain requests
+  timeout: 300000 // request timeout
+})
+
+// response interceptor
+service.interceptors.response.use(response => {
+  return response.data
+})
 
 export interface ResData<T> {
   code: number,
@@ -15,7 +26,7 @@ export interface ResData<T> {
 }
 
 const request1 = <T>(url: string, method: Method = 'GET', params: object, data: object) => {
-  return myRequest<any, ResData<T>>({
+  return service<any, ResData<T>>({
     url,
     method,
     params,
@@ -24,5 +35,5 @@ const request1 = <T>(url: string, method: Method = 'GET', params: object, data: 
 }
 
 export default <T>(params:any) => {
-  return request1<T>(params.url, params.method, params.params, params.data)
+  return request1<T>(params.url, params.method, params.params || {}, params.data || {})
 }
